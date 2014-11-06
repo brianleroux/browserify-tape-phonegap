6627ce74a9c631fae70f455973643eb5f54c4f21
brianleroux <b@brian.io>
2014-11-06 14:52:57 +0000

    maybe get a badge?!

--- test/test-sanity.js
+++ test/test-sanity.js
@@ -3,11 +3,10 @@ var test = require('tape')
   , report = require('browserify-tape-spec')
 
 test('useful assertion description', function(t) {
-  t.plan(2)
+  t.plan(1)
   t.ok('a passing test')
-  t.fail('this test will fail')
+//  t.fail('this test will fail')
   t.end()
 })
 
-test.createStream().pipe(report('out'))
 
